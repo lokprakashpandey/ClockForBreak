cb22088aa1ea2b71b32874d2405dfc6b392ffa37
Lok Prakash Pandey <lokprakashpandey@yahoo.com>
2018-05-18 22:44:44 -0400

    Add files via upload
    Just use space in project name.......OK

---
+++ Clock For Break/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Clock_For_Break
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("DS-Digital", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblTime.Location = new System.Drawing.Point(320, 200);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(257, 95);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "20:00";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTime.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(911, 525);
+            this.Controls.Add(this.lblTime);
+            this.ForeColor = System.Drawing.Color.Cyan;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clock for Break Time";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ Clock For Break/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock_For_Break
+{
+    
+    public partial class Form1 : Form
+    {
+        Timer t;
+        int m=20, s=0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new Timer();
+
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.t_Tick);
+            this.UseWaitCursor = true;
+            t.Start();
+            
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            if(m == 0)
+            {
+                lblTime.Text = "Time Up";
+                return;
+            }
+            if (s == 0)
+            {
+                m -= 1;
+                s = 59;
+            }
+            else
+            {
+                s -= 1;
+            }
+
+            
+            string time = "";
+
+            if (m<10)
+            {
+                time += "0" + m;
+            }
+            else
+            {
+                time += m;
+            }
+
+            time += ":";
+
+            if (s<10)
+            {
+                time += "0" + s;
+            }
+            else
+            {
+                time += s;
+            }
+
+            lblTime.Text = time;
+            //System.Diagnostics.Process.Start(@"C:\WINDOWS\system32\rundll32.exe", "user32.dll,LockWorkStation");
+
+        }
+    }
+}
